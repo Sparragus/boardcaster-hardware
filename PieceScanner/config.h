d985d487f632f191d2de475bc3e358855333e4a0
dlcs.frank-ece <dlcs.frank@gmail.com>
2011-10-16 23:06:04 -0400

    Added missing config.h

---
+++ PieceScanner/config.h
@@ -0,0 +1,27 @@
+#define WIDTH 8
+#define HEIGHT 8
+
+// MUX = Columns
+// DECs = ROWS
+#define MUX_PA 7
+#define MUX_PB 6
+#define MUX_PC 5
+	
+#define DEC_PA 4
+#define DEC_PB 3
+#define DEC_PC 2
+		
+#define OUT_DATA 8
+
+const long unsigned TIME_SETTLE = 200;
+const long unsigned TIME_NEXT = 10;
+
+// Hold all the states
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+struct sig_t {
+  uchar PORT1;
+  uchar PORT2;
+  uchar PORT3;
+} sig;
