3f3adf8ac7cf983e2a0a12ceed4281f99e5be34f
xerces <dlcs.frank@gmail.com>
2011-10-30 00:03:36 -0400

    Moved to a 64 sensor display. Added better comments for wiring. [HW] Fixed decoder wiring bug.

--- boardcaster/piece_detector.h
+++ boardcaster/piece_detector.h
@@ -4,23 +4,23 @@
 
 // MUX = Columns
 // DECs = ROWS
-#define MUX_PA 7
-#define MUX_PB 6
-#define MUX_PC 5
+#define MUX_PA 7  // MSB // C - Pin9
+#define MUX_PB 6         // B - Pin10
+#define MUX_PC 5 // LSB  // A - Pin11
 	
-#define DEC_PA0 4
-#define DEC_PA1 3
-#define DEC_PA2 2
+#define DEC_PA0 4 // MSB // A2 - Pin3
+#define DEC_PA1 3        // A1 - Pin2
+#define DEC_PA2 2 // LSB // A0 - Pin1
 		
 #define OUT_DATA 8
 
 // Under non-debugging operation SENSOR_COUNT = SCAN_SIZE = 64
 #define SCAN_SIZE 64
-#define SENSOR_COUNT 6
+#define SENSOR_COUNT 64
 
 #define PRINT_RES 0
 #define PRINT_TIME 0
-
+//#define DEBUG
 typedef unsigned long long uint64_t;
 // Temporary board for delta calculation
 extern uint64_t old_board;
