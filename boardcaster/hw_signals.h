fcd221bf3b0b64a95f45b6462d94a05af0a23251
xerces <dlcs.frank@gmail.com>
2011-10-30 00:15:58 -0400

    Added missing macros.

--- boardcaster/hw_signals.h
+++ boardcaster/hw_signals.h
@@ -1,4 +1,9 @@
+#ifndef HW_SIGNALS_H
+#define HW_SIGNALS_H
+
 void generateSig(uchar i, sig_t* s);
 void setDecoder(sig_t* s);
 void setMux(sig_t* s);
 int readPieceArrayLine();
+
+#endif // HW_SIGNALS_H
--- boardcaster/utils.h
+++ boardcaster/utils.h
@@ -1,1 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 void linTo2D(uchar i, uchar* x, uchar* y);
+
+#endif //
