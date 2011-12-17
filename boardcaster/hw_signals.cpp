e40a9ca08a9279cfa9b5721059556e699ec5ce59
xerces <dlcs.frank@gmail.com>
2011-12-17 13:24:13 -0400

    Improved comments for hw_signals

--- boardcaster/hw_signals.cpp
+++ boardcaster/hw_signals.cpp
@@ -1,29 +1,30 @@
 /*Copyright (C) 2011  Francisco De La Cruz, Gabriel J. Pérez
-and Richard B. Kaufman
+  and Richard B. Kaufman
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #include <WProgram.h>
 #include "piece_detector.h"
 #include "defines.h"
+
 // Get result from the piece array
 int readPieceArrayLine()
 {
     return digitalRead(PD_OUT_DATA);
 }
 
-// Set decoder to the given signal
+// Set decoder to the given signal structure
 void setDecoder(sig_t* s)
 {
     // 74HCT138 Decoder uses reversed select pins as well
@@ -39,6 +40,7 @@ void setDecoder(sig_t* s)
     Serial.print(",");
     Serial.println(s->m_port3,DEC);
 #endif
+    // Remember to swap the MSB and LSB
     digitalWrite(PD_DEC_PA2, s->m_port1);
     digitalWrite(PD_DEC_PA1, s->m_port2);
     digitalWrite(PD_DEC_PA0, s->m_port3);
@@ -64,7 +66,7 @@ void setMux(sig_t* s)
 
     */
    
- // HACK: Invert multiplexer signal
+    // HACK: Invert multiplexer signal
     s->m_port1 = (uchar)(!(s->m_port1));
     s->m_port2 = (uchar)(!(s->m_port2));
     s->m_port3 = (uchar)(!(s->m_port3));
@@ -77,8 +79,6 @@ void setMux(sig_t* s)
     Serial.print(",");
     Serial.println(s->m_port3,DEC);
 #endif
-    
-   
     
     digitalWrite(PD_MUX_PC, s->m_port1);
     digitalWrite(PD_MUX_PB, s->m_port2);
