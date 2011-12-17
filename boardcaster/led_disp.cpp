682e3ceeaa1e2b4cf77e64506094cd4480e5905b
xerces <dlcs.frank@gmail.com>
2011-12-17 13:27:50 -0400

    Improved comments for led_disp

--- boardcaster/led_disp.cpp
+++ boardcaster/led_disp.cpp
@@ -1,18 +1,18 @@
 /*Copyright (C) 2011  Francisco De La Cruz, Gabriel J. Pérez
-and Richard B. Kaufman
+  and Richard B. Kaufman
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #include <WProgram.h>
 #include "led_disp.h"
@@ -23,39 +23,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #include "utils.h"
 
 // Display the letters B and C to test the display
+// This is usually flashed upon successfully connecting to
+// the Boardcaster website
 void flashOK()
 {
-  uchar o_arr[] = {
-    0,1,1,1,1,1,0,0,
-    0,1,0,0,0,0,1,0,
-    0,1,0,0,0,0,1,0,
-    0,1,1,1,1,1,1,0,
-    0,1,0,0,0,0,1,0,
-    0,1,0,0,0,0,1,0,
-    0,1,0,0,0,0,1,0,
-    0,1,1,1,1,1,0,0,
-  };
-  uchar k_arr[] = {
-    0,0,1,1,1,1,1,0,
-    0,1,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,
-    1,0,0,0,0,0,0,0,
-    1,0,0,0,0,0,0,0,
-    1,0,0,0,0,0,0,0,
-    0,1,0,0,0,0,0,1,
-    0,0,1,1,1,1,1,0,
-  };
-  uint64_t O;
-  uint64_t K;
-  arrayToBitBoard(o_arr, &O);
-  arrayToBitBoard(k_arr, &K);
-  clearDisplay();
-  displayPositions(&O);
-  delay(1500);
-  clearDisplay();
-  displayPositions(&K);
-  delay(1500);
-  clearDisplay();
+    uchar o_arr[] = {
+        0,1,1,1,1,1,0,0,
+        0,1,0,0,0,0,1,0,
+        0,1,0,0,0,0,1,0,
+        0,1,1,1,1,1,1,0,
+        0,1,0,0,0,0,1,0,
+        0,1,0,0,0,0,1,0,
+        0,1,0,0,0,0,1,0,
+        0,1,1,1,1,1,0,0,
+    };
+    uchar k_arr[] = {
+        0,0,1,1,1,1,1,0,
+        0,1,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,0,
+        1,0,0,0,0,0,0,0,
+        1,0,0,0,0,0,0,0,
+        1,0,0,0,0,0,0,0,
+        0,1,0,0,0,0,0,1,
+        0,0,1,1,1,1,1,0,
+    };
+    uint64_t O;
+    uint64_t K;
+    
+    // Make into bitboards
+    arrayToBitBoard(o_arr, &O);
+    arrayToBitBoard(k_arr, &K);
+    
+    // Flash the letters
+    clearDisplay();
+    displayPositions(&O);
+    delay(1500);
+    clearDisplay();
+    displayPositions(&K);
+    delay(1500);
+    clearDisplay();
 }
 
 // Initialize the pins to be used for the shift registers
@@ -70,6 +76,8 @@ void initLedDisp()
     digitalWrite(LA_LATCH_PIN, HIGH);
 }
 
+// Shift out the 16bits in val over dataPin
+// Toggle clockPin and use bitOrder first
 void shiftOut16(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint16_t val)
 {
     uint16_t i;
@@ -89,6 +97,7 @@ void shiftOut16(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint16_t va
 // is necessarry after this call to turn off the positions.
 void displayPositions(const uint64_t* inBoard)
 {
+    // Clear the display first
     clearDisplay();
    
     // Create a copy of the board 
@@ -102,6 +111,7 @@ void displayPositions(const uint64_t* inBoard)
     // with the mirrored board. 
     mirrorBitboardX(&inBoardNC);
 
+    // Set the board parts to serial out
     board_parts[0] = inBoardNC;
     board_parts[1] = (inBoardNC >> 16);
     board_parts[2] = (inBoardNC >> 32);
@@ -115,6 +125,7 @@ void displayPositions(const uint64_t* inBoard)
 
     // Set the clockPin low to prepare for rising edge trigger
     digitalWrite(LA_CLOCK_PIN, LOW);  
+
     // Shift out the data
     for(int i = 0; i < 4; i++)
     {
@@ -126,6 +137,9 @@ void displayPositions(const uint64_t* inBoard)
     // Set LA_LATCH_PIN low to allow data to pass through
     digitalWrite(LA_LATCH_PIN, HIGH);
     digitalWrite(LA_XEN_PIN, LOW);
+
+    lockDisplay();
+
 }
 
 // Clear all data in shift registers to clear display
@@ -139,7 +153,8 @@ void clearDisplay()
 
     // Set the clockPin low to prepare for rising edge trigger
     digitalWrite(LA_CLOCK_PIN, LOW);  
-    // Shift out the data
+
+    // Just shift out zeros
     for(int i = 0; i < 4; i++)
     {
         shiftOut16(LA_DATA_PIN, LA_CLOCK_PIN, LSBFIRST, 0);
