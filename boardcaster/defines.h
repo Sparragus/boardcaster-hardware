970e0d72958058df774bfbcd8d70fb76160dad21
xerces <dlcs.frank@gmail.com>
2011-12-17 13:21:57 -0400

    Cleaned up boardcaster and defines headers.

--- boardcaster/defines.h
+++ boardcaster/defines.h
@@ -1,18 +1,18 @@
 /*Copyright (C) 2011  Francisco De La Cruz, Gabriel J. Pérez
-and Richard B. Kaufman
+  and Richard B. Kaufman
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #ifndef _DEFINES_H
 #define _DEFINES_H
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 // ------------
 
 // LED Array
+// ---------
 //Pin connected to S-IN of IR2D07s
 #define LA_DATA_PIN A0
 //Pin connected to CLK of IR2D07s
@@ -31,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #define LA_XEN_PIN A3
 
 // Piece detector
-
+// -----------
 // MUX = Columns
 // DECs = ROWS
 #define PD_MUX_PA 36  // LSB //
@@ -42,6 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #define PD_DEC_PA1 40       //
 #define PD_DEC_PA2 38 // MSB //
 		
+// PCB Out = Arduino IN
 #define PD_OUT_DATA 30
 
 
