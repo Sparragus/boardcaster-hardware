0aa878befc92b6556f92639da30ee845a2a5106d
xerces <dlcs.frank@gmail.com>
2011-12-17 13:29:56 -0400

    Improved comments for MemoryFree and added forum source URL

--- boardcaster/MemoryFree.cpp
+++ boardcaster/MemoryFree.cpp
@@ -1,34 +1,36 @@
 /*Copyright (C) 2011  Francisco De La Cruz, Gabriel J. Pérez
-and Richard B. Kaufman
+  and Richard B. Kaufman
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+// Based on code provided by user "mem"
+// at http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1213583720/15
+
 
 extern unsigned int __bss_end;
 extern unsigned int __heap_start;
 extern void *__brkval;
 
-
 #include "MemoryFree.h";
 
-
 int freeMemory() {
-  int free_memory;
+    int free_memory;
 
-  if((int)__brkval == 0)
-     free_memory = ((int)&free_memory) - ((int)&__bss_end);
-  else
-    free_memory = ((int)&free_memory) - ((int)__brkval);
+    if((int)__brkval == 0)
+        free_memory = ((int)&free_memory) - ((int)&__bss_end);
+    else
+        free_memory = ((int)&free_memory) - ((int)__brkval);
 
-  return free_memory;
+    return free_memory;
 }
--- boardcaster/MemoryFree.h
+++ boardcaster/MemoryFree.h
@@ -1,20 +1,21 @@
 /*Copyright (C) 2011  Francisco De La Cruz, Gabriel J. Pérez
-and Richard B. Kaufman
+  and Richard B. Kaufman
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
-// memoryFree header
+// Based on code provided by user "mem"
+// at http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1213583720/15
 
 #ifndef	MEMORY_FREE_H
 #define MEMORY_FREE_H
@@ -22,9 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-int freeMemory();
-
+    int freeMemory();
 #ifdef  __cplusplus
 }
 #endif
